f02f0a38a02ba1f2de99e1faf8370e1a422379cb
konson-is <i.shoji.0731@gmail.com>
2023-09-05 11:39:21 +0900

    blue/greenのトラフィックの切り替え時間の変更

--- code_series.tf
+++ code_series.tf
@@ -46,7 +46,7 @@ resource "aws_codedeploy_deployment_group" "sbcntr-backend-ecs-deploy-group" {
     blue_green_deployment_config {
       deployment_ready_option {
         action_on_timeout = "STOP_DEPLOYMENT"
-        wait_time_in_minutes = 60
+        wait_time_in_minutes = 10
       }
 
       terminate_blue_instances_on_deployment_success {
